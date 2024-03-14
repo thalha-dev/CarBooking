90ad584bd365ddb3a9a7fb6e6d5afa700600b46e
thalha-dev <dev.thalha@gmail.com>
2024-03-14 11:17:34 +0530

    setup db

---
+++ src/main/java/com/learn/CarBooking/CarBookingApplication.java
@@ -0,0 +1,13 @@
+package com.learn.CarBooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarBookingApplication.class, args);
+	}
+
+}
